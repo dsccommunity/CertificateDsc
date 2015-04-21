d612178d6a38254213ee834727fbb08951223dd2
mgreenegit <migreene@outlook.com>
2015-04-20 21:45:02 -0400

    First commits from development.

---
+++ Resources/Example.ps1
@@ -0,0 +1,33 @@
+﻿configuration SSL
+{
+    param (
+        [Parameter(Mandatory=$true)] 
+        [ValidateNotNullorEmpty()] 
+        [PsCredential] $Credential 
+        )
+    Import-DscResource -ModuleName xCertificate
+    Node 'localhost'
+    {
+	    xCertReq SSLCert
+	    {
+		    CARootName                = 'test-dc01-ca'
+		    CAServerFQDN              = 'dc01.test.pha'
+		    Subject                   = 'foodomain.test.net'
+		    AutoRenew                 = $true
+		    Credential                = $Credential
+	    }
+    }
+}
+$configData = @{
+    AllNodes = @(
+        @{
+            NodeName                    = 'localhost';
+            PSDscAllowPlainTextPassword = $true
+            }
+        )
+    }
+SSL -ConfigurationData $configData -Credential (get-credential) -OutputPath 'c:\SSLConfig'
+Start-DscConfiguration -Wait -Force -Verbose -Path 'c:\SSLConfig'
+
+# Validate results
+Get-ChildItem Cert:\LocalMachine\My
