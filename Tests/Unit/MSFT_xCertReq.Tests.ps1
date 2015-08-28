d229297326b86fdae5bb42dd54703995ffd564c8
Dan1el42 <Dan1el42@users.noreply.github.com>
2015-08-27 20:18:17 -0600

    Basic schema validation tests for MSFT_xCertReq. Updated appveyor.yml to create nuget package and moved build number to third section of version.

---
+++ Tests/Unit/MSFT_xCertReq.Tests.ps1
@@ -0,0 +1,33 @@
+﻿#Requires -RunAsAdministrator
+#Requires -Modules xDSCResourceDesigner
+
+$DSCResourceName = 'MSFT_xCertReq'
+
+$Splat = @{
+    Path = $PSScriptRoot
+    ChildPath = "..\..\DSCResources\$DSCResourceName\$DSCResourceName.psm1"
+    Resolve = $true
+    ErrorAction = 'Stop'
+}
+$DSCResourceModuleFile = Get-Item -Path (Join-Path @Splat)
+
+Describe "Schema Validation $DSCResourceName" {
+    It 'should pass Test-xDscResource' {
+        $result = Test-xDscResource -Name $DSCResourceModuleFile.DirectoryName
+        $result | Should Be $true
+    }
+
+    It 'should pass Test-xDscSchema' {
+        $Splat = @{
+            Path = $DSCResourceModuleFile.DirectoryName
+            ChildPath = "$($DSCResourceName).schema.mof"
+        }
+        $result = Test-xDscSchema -Path (Join-Path @Splat -Resolve -ErrorAction Stop)
+        $result | Should Be $true
+    }
+}
+
+if (Get-Module -Name $DSCResourceName)
+{
+    Remove-Module -Name $DSCResourceName
+}
