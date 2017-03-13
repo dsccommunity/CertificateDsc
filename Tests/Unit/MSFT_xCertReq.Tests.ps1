f3acbb025af12df36b8f4e53b274b0f94657651d
Sven van Rijen <sven@svenvanrijen.nl>
2017-03-13 15:31:52 +0100

    Adjusted unit tests

--- Tests/Unit/MSFT_xCertReq.Tests.ps1
+++ Tests/Unit/MSFT_xCertReq.Tests.ps1
@@ -535,10 +535,10 @@ RenewalCert = $validThumbprint
                 Mock -CommandName Import-Module
 
                 function Start-Win32Process { param ( $Path,$Arguments,$Credential ) }
-                function Wait-Win32ProcessEnd { param ( $Path,$Arguments,$Credential ) }
+                function Wait-Win32ProcessStop { param ( $Path,$Arguments,$Credential ) }
 
                 Mock -CommandName Start-Win32Process -ModuleName MSFT_xCertReq
-                Mock -CommandName Wait-Win32ProcessEnd -ModuleName MSFT_xCertReq
+                Mock -CommandName Wait-Win32ProcessStop -ModuleName MSFT_xCertReq
 
                 It 'should not throw' {
                     { Set-TargetResource @Params } | Should Not Throw
