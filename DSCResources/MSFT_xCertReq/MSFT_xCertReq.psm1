39acb5b6acf8638acda04257de34d323edb9102d
Sean Gray <sean.e.gray@outlook.com>
2017-05-12 12:39:08 +0100

    Replace tabs with spaces

--- DSCResources/MSFT_xCertReq/MSFT_xCertReq.psm1
+++ DSCResources/MSFT_xCertReq/MSFT_xCertReq.psm1
@@ -751,36 +751,36 @@ function Test-TargetResource
             } # if
         } # if
 
-		if ($PSBoundParameters.ContainsKey('SubjectAltName')) {
-			# Split the desired SANs into an array
-			$SanList = $SubjectAltName.Split('&')
-			$CorrectDNS = @()
-			foreach ($San in $SanList) {
-				if ($San -like 'dns*') {
-					# This SAN is a DNS name
-					$CorrectDNS += $San.split('=')[1]
-				}
-			}
-			# Convert array to string (makes comparison easy)
-			$CorrectDNSString = ($CorrectDNS | Sort-Object | Get-Unique) -join ','
-			
-			# Find out what SANs are on the current cert
-			$CurrentSanList = ($cert.Extensions | Where {$_.oid.FriendlyName -match 'Subject Alternative Name'}).Format(1).split("`n").TrimEnd()
-			$CurrentDNS = @()
-			foreach ($San in $CurrentSanList) {
-				if ($San -like 'dns*') {
-					# This SAN is a DNS name
-					$CurrentDNS += $San.split('=')[1]
-				}
-			}
-			# Convert array to string (makes comparison easy)
-			$CurrentDNSString = ($CurrentDNS | Sort-Object | Get-Unique) -join ','
-			
-			# Do the cert's DNS SANs and the desired DNS SANs match?
-			if ($CurrentDNSString -ne $CorrectDNSString) {
-				return $false
-			}
-		}
+        if ($PSBoundParameters.ContainsKey('SubjectAltName')) {
+            # Split the desired SANs into an array
+            $SanList = $SubjectAltName.Split('&')
+            $CorrectDNS = @()
+            foreach ($San in $SanList) {
+                if ($San -like 'dns*') {
+                    # This SAN is a DNS name
+                    $CorrectDNS += $San.split('=')[1]
+                }
+            }
+            # Convert array to string (makes comparison easy)
+            $CorrectDNSString = ($CorrectDNS | Sort-Object | Get-Unique) -join ','
+            
+            # Find out what SANs are on the current cert
+            $CurrentSanList = ($cert.Extensions | Where {$_.oid.FriendlyName -match 'Subject Alternative Name'}).Format(1).split("`n").TrimEnd()
+            $CurrentDNS = @()
+            foreach ($San in $CurrentSanList) {
+                if ($San -like 'dns*') {
+                    # This SAN is a DNS name
+                    $CurrentDNS += $San.split('=')[1]
+                }
+            }
+            # Convert array to string (makes comparison easy)
+            $CurrentDNSString = ($CurrentDNS | Sort-Object | Get-Unique) -join ','
+            
+            # Do the cert's DNS SANs and the desired DNS SANs match?
+            if ($CurrentDNSString -ne $CorrectDNSString) {
+                return $false
+            }
+        }
 
         # The certificate was found and is OK - so no change required.
         Write-Verbose -Message ( @(
