bcf59c9d03a5f8a9eca0946b05104bebde5d2593
Tim Haintz <timhaintz@hotmail.com>
2018-03-04 10:48:35 +1100

    Changed xCertificate to CertificateDsc in file path.

--- .vscode/RunAllTests.ps1
+++ .vscode/RunAllTests.ps1
@@ -2,10 +2,10 @@
 if ( (-not (Test-Path -Path (Join-Path -Path $repoRoot -ChildPath 'DSCResource.Tests'))) -or `
     (-not (Test-Path -Path (Join-Path -Path $repoRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
 {
-    & git @('clone', 'https://github.com/PowerShell/DscResource.Tests.git', (Join-Path -Path $repoRoot -ChildPath '\Modules\xCertificate\DSCResource.Tests\'))
+    & git @('clone', 'https://github.com/PowerShell/DscResource.Tests.git', (Join-Path -Path $repoRoot -ChildPath '\Modules\CertificateDsc\DSCResource.Tests\'))
 }
 
 Import-Module (Join-Path $repoRoot "\Tests\TestHarness.psm1" -Resolve)
 $dscTestsPath = Join-Path -Path $repoRoot `
-    -ChildPath "\Modules\xCertificate\DscResource.Tests\Meta.Tests.ps1"
+    -ChildPath "\Modules\CertificateDsc\DscResource.Tests\Meta.Tests.ps1"
 Invoke-TestHarness -DscTestsPath $dscTestsPath
