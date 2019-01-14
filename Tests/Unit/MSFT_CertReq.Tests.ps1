830ed283645fc1b88f3c689b25ed09c201045d1a
rchristman <ryan.christman@microsoft.com>
2019-01-14 11:31:24 -0500

    Testing removing helper function Import.

--- Tests/Unit/MSFT_CertReq.Tests.ps1
+++ Tests/Unit/MSFT_CertReq.Tests.ps1
@@ -15,7 +15,7 @@ if ( (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCR
 }
 
 Import-Module (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1') -Force
-Import-Module (Join-Path -Path $script:moduleRoot -ChildPath '\Tests\TestHelpers\CommonTestHelper.psm1') -Force
+
 $TestEnvironment = Initialize-TestEnvironment `
     -DSCModuleName $script:DSCModuleName `
     -DSCResourceName $script:DSCResourceName `
