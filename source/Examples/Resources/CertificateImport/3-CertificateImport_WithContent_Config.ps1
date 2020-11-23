a531ac094240563d938aecac6e817f298f0949f7
Michael Beggs <switch21@gmail.com>
2020-11-23 17:05:07 -0500

    Update example

---
+++ source/Examples/Resources/CertificateImport/3-CertificateImport_WithContent_Config.ps1
@@ -0,0 +1,43 @@
+<#PSScriptInfo
+.VERSION 1.0.0
+.GUID f35aa0ac-1b22-4309-89aa-05618419e7b9
+.AUTHOR DSC Community
+.COMPANYNAME DSC Community
+.COPYRIGHT Copyright the DSC Community contributors. All rights reserved.
+.TAGS DSCConfiguration
+.LICENSEURI https://github.com/dsccommunity/CertificateDsc/blob/master/LICENSE
+.PROJECTURI https://github.com/dsccommunity/CertificateDsc
+.ICONURI
+.EXTERNALMODULEDEPENDENCIES
+.REQUIREDSCRIPTS
+.EXTERNALSCRIPTDEPENDENCIES
+.RELEASENOTES First version.
+.PRIVATEDATA 2016-Datacenter,2016-Datacenter-Server-Core
+#>
+
+#Requires -module CertificateDsc
+
+$contentByte = Get-Content -Path D:\MyTrustedRoot.cer -Encoding Byte
+$contentBase64 = ([System.Convert]::ToBase64String($contentByte))
+
+<#
+    .DESCRIPTION
+        Import public key certificate into Trusted Root store from
+        a provided base64 encoded string.
+#>
+Configuration CertificateImport_WithContent_Config
+{
+    Import-DscResource -ModuleName CertificateDsc
+
+    Node localhost
+    {
+        CertificateImport MyTrustedRoot
+        {
+            Thumbprint   = 'c81b94933420221a7ac004a90242d8b1d3e5070d'
+            Location     = 'LocalMachine'
+            Store        = 'Root'
+            Path         = 'C:\Windows\Temp\MyTrustedRoot.cer'
+            Content      = $contentBase64
+        }
+    }
+}
---
+++ source/Examples/Resources/PfxImport/6-PfxImport_InstallPFXFromContent_Config.ps1
@@ -0,0 +1,51 @@
+<#PSScriptInfo
+.VERSION 1.0.0
+.GUID a93bd794-e0c1-45fb-a988-c637257d4b35
+.AUTHOR DSC Community
+.COMPANYNAME DSC Community
+.COPYRIGHT Copyright the DSC Community contributors. All rights reserved.
+.TAGS DSCConfiguration
+.LICENSEURI https://github.com/dsccommunity/CertificateDsc/blob/master/LICENSE
+.PROJECTURI https://github.com/dsccommunity/CertificateDsc
+.ICONURI
+.EXTERNALMODULEDEPENDENCIES
+.REQUIREDSCRIPTS
+.EXTERNALSCRIPTDEPENDENCIES
+.RELEASENOTES First version.
+.PRIVATEDATA 2016-Datacenter,2016-Datacenter-Server-Core
+#>
+
+#Requires -Modules CertificateDsc
+
+$contentByte = Get-Content -Path D:\CompanyCert.pfx -Encoding Byte
+$contentBase64 = ([System.Convert]::ToBase64String($contentByte))
+
+<#
+    .DESCRIPTION
+        Import a PFX into the 'My' Local Machine certificate store.
+#>
+Configuration PfxImport_InstallPFXFromContent_Config
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullorEmpty()]
+        [System.Management.Automation.PSCredential]
+        $Credential
+    )
+
+    Import-DscResource -ModuleName CertificateDsc
+
+    Node localhost
+    {
+        PfxImport CompanyCert
+        {
+            Thumbprint = 'c81b94933420221a7ac004a90242d8b1d3e5070d'
+            Path       = 'C:\Windows\Temp\CompanyCert.pfx'
+            Content    = $contentBase64
+            Location   = 'LocalMachine'
+            Store      = 'My'
+            Credential = $Credential
+        }
+    }
+}
